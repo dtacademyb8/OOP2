d5add964851f2aebd633a0b6c63b73e90b4c9ff0
Hussain Naeem <huss9k@gmail.com>
2022-06-03 21:10:47 -0400

    third

---
+++ src/Utility.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Utility{
+
+  public static List<String[]> readFromCSV(String str) throws IOException{
+    /// Comment to check
+  List<String[]> result = new ArrayList<>();
+    try{
+    BufferedReader br = new BufferedReader(new FileReader(str));
+    
+    String strConv;
+    
+    while((strConv=br.readLine()) !=null){
+      String[] eachrow = strConv.split(",");
+      result.add(eachrow);
+    }
+      
+    } catch (IOException e){
+      System.out.println("Exception");
+      }
+    return result; 
+    //System.out.println(result.toString());
+    
+    //System.out.println(result.toString());
+    
+  }
+  
+}
